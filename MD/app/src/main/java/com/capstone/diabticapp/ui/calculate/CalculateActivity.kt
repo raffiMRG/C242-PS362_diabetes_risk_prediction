dd3b53f7a34d05ce408f9f2d3b7f3ecf7c8856db
raffiMRG <mutekinoraffi@gmail.com>
2024-11-20 03:54:15 +0700

    add font, appBar & fixing news

--- MD/app/src/main/java/com/capstone/diabticapp/ui/calculate/CalculateActivity.kt
+++ MD/app/src/main/java/com/capstone/diabticapp/ui/calculate/CalculateActivity.kt
@@ -1,6 +1,8 @@
 package com.capstone.diabticapp.ui.calculate
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +19,8 @@ class CalculateActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-
+//        myAppBar.setOnBackPressedListener{
+//            onBackPressed()
+//        }
     }
 }
